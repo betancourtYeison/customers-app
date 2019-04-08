9c8bb0ca513f7c7146399cfeb6b4f77598c34614
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-08 16:55:15 -0500

    Add redux form to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { customers } from './customers';
+import { reducer as form } from 'redux-form';
 
 export default combineReducers({
-    customers
+    customers,
+    form,
 })
