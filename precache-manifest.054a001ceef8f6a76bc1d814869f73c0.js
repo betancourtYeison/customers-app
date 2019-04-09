d10fc70fc4b7cc2c5b283c6c25656b018f0181fb
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-09 16:33:41 -0500

    Updates

--- precache-manifest.054a001ceef8f6a76bc1d814869f73c0.js
+++ precache-manifest.054a001ceef8f6a76bc1d814869f73c0.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/customers-app/static/js/runtime~main.a401290d.js"
   },
   {
-    "revision": "dce7e620444b27c2d9ee",
-    "url": "/customers-app/static/js/main.5eb09d0f.chunk.js"
+    "revision": "3bc7bcaae5bc987a0a2b",
+    "url": "/customers-app/static/js/main.50cc7f03.chunk.js"
   },
   {
     "revision": "f8f4195c1e20563bc653",
     "url": "/customers-app/static/js/2.590fe88f.chunk.js"
   },
   {
-    "revision": "dce7e620444b27c2d9ee",
+    "revision": "3bc7bcaae5bc987a0a2b",
     "url": "/customers-app/static/css/main.caee6abb.chunk.css"
   },
   {
-    "revision": "1c23c16cdf91d712e3e579e46872358c",
+    "revision": "1945a9944b6b726ed180a3568e7f9c95",
     "url": "/customers-app/index.html"
   }
 ];
