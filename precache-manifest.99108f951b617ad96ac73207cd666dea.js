a2a305ff068feca188f130570305b7b6412d1d5a
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-09 15:38:53 -0500

    Updates

---
+++ precache-manifest.99108f951b617ad96ac73207cd666dea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "betancourtYeison.github.io/customers-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d4c065e3bb72611c0beb",
+    "url": "betancourtYeison.github.io/customers-app/static/js/runtime~main.6f4494d2.js"
+  },
+  {
+    "revision": "2cdf8a9d80213d30262c",
+    "url": "betancourtYeison.github.io/customers-app/static/js/main.e895f35f.chunk.js"
+  },
+  {
+    "revision": "f8f4195c1e20563bc653",
+    "url": "betancourtYeison.github.io/customers-app/static/js/2.590fe88f.chunk.js"
+  },
+  {
+    "revision": "2cdf8a9d80213d30262c",
+    "url": "betancourtYeison.github.io/customers-app/static/css/main.caee6abb.chunk.css"
+  },
+  {
+    "revision": "c244767e222e4d2787aa2a0cac922bbc",
+    "url": "betancourtYeison.github.io/customers-app/index.html"
+  }
+];
