925765adb72032218aebd0827047b602e5d764e1
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-04-09 13:17:02 -0500

    Add reducer to update customer

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,7 +1,18 @@
 import { handleActions } from 'redux-actions';
-import { FETCH_CUSTOMERS, INSERT_CUSTOMER } from '../constants';
+import { FETCH_CUSTOMERS, UPDATE_CUSTOMER, INSERT_CUSTOMER } from '../constants';
 
 export const customers = handleActions({
     [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
+    [UPDATE_CUSTOMER]: (state, action) => {
+        const newCustomer = action.payload;
+        const newCustomers = state.reduce((acc, customer) => {
+            if (customer.id === newCustomer.id){
+                return [...acc, newCustomer];
+            }else{
+                return [...acc, customer];
+            }
+        }, []);
+        return newCustomers;
+    },
     [INSERT_CUSTOMER]: (state, action) => [...state, action.payload]
 }, []);
